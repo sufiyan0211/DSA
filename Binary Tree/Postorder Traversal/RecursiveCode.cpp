5b27e84e005f01864d2d6d4e4d4324610121f520
Mohammed Abdul Sofiyan <sofiyan@Mohammeds-MacBook-Pro.local>
2023-01-05 00:35:06 +0530

    Postorder Traversal

---
+++ Binary Tree/Postorder Traversal/RecursiveCode.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+public:
+	int key;
+	Node *left;
+	Node *right;
+	Node(int key) {
+		this->key = key;
+		left = right = NULL;
+	}
+};
+
+void postorder(Node *root) {
+	if(root != NULL) {
+		postorder(root->left);
+		postorder(root->right);
+		cout << root->key << " ";
+		
+	}
+}
+/* Binary Tree in the input
+			10
+		   /   \
+		 20     30
+		/ \     / \
+	  40   50  60  70
+*/
+
+int main() {
+	Node *root = new Node(10);
+	root->left = new Node(20);
+	root->right = new Node(30);
+	root->left->left = new Node(40);
+	root->left->right = new Node(50);
+	root->right->left = new Node(60);
+	root->right->right = new Node(70);
+
+	postorder(root);
+	cout << endl;
+
+	return 0;
+}
