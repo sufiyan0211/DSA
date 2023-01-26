017d4811674e5b3bb0a71d83e0f3c666939112e2
Mohammed Abdul Sofiyan <sofiyan@Mohammeds-MacBook-Pro.local>
2023-01-26 19:40:08 +0530

    Update Graph and its Edges

--- Graph/BFS/Code.cpp
+++ Graph/BFS/Code.cpp
@@ -19,21 +19,21 @@ void dfs(vector<vector<int>> &adj, vector<int> &ans, int s, vector<bool> &visite
 
 
 int main() {
-	int v = 5;
+	int v = 4;
 	vector<vector<int>> adj(v);
 	addEdge(adj, 0, 1);
 	addEdge(adj, 0, 2);
-	addEdge(adj, 1, 0);
-	addEdge(adj, 1, 3);
-	addEdge(adj, 1, 4);
-	addEdge(adj, 2, 0);
-	addEdge(adj, 2, 3);
-	addEdge(adj, 3, 2);
-	addEdge(adj, 3, 1);
-	addEdge(adj, 3, 4);
 
-	addEdge(adj, 4, 1);
-	addEdge(adj, 4, 3);
+	addEdge(adj, 1, 0);
+	addEdge(adj, 1, 2);
+	addEdge(adj, 1, 3);
+
+	addEdge(adj, 2, 0);
+	addEdge(adj, 2, 1);
+	addEdge(adj, 2, 3);
+
+	addEdge(adj, 3, 1);
+	addEdge(adj, 3, 2);
 
 	vector<int> ans;
 	vector<bool> visited(v, false);
