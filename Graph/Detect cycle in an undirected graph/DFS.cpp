ff8efbd647b84aaf3964e95bcf1df7f03444cdee
Mohammed Abdul Sofiyan <sofiyan@Mohammeds-MacBook-Pro.local>
2023-01-26 20:30:57 +0530

    Detect cycle in an undirected graph

---
+++ Graph/Detect cycle in an undirected graph/DFS.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void addEdge(vector<vector<int>> &adj, int v, int u) {
+	adj[u].push_back(v);
+	adj[v].push_back(u);
+}
+
+bool dfs(vector<vector<int>> &adj, vector<bool> &visited, int src, int parent) {
+	
+	visited[src] = true;
+
+	for(auto v: adj[src]) {
+		if(visited[v] == false) {
+			bool isCyclePresent = dfs(adj, visited, v, src);
+			if(isCyclePresent) return true;
+		}
+		else if(v != parent) {
+			return true;
+		}
+	}
+	return false;
+}
+
+bool isCycleDetect(vector<vector<int>> &adj, vector<bool> &visited, int src) {
+	return dfs(adj, visited, src, -1);
+}
+
+
+int main() {
+	int v = 4;
+	vector<vector<int>> adj(v);
+	addEdge(adj, 0, 1);
+	addEdge(adj, 0, 2);
+
+	addEdge(adj, 1, 0);
+	addEdge(adj, 1, 2);
+	addEdge(adj, 1, 3);
+
+	addEdge(adj, 2, 0);
+	addEdge(adj, 2, 1);
+	addEdge(adj, 2, 3);
+
+	addEdge(adj, 3, 1);
+	addEdge(adj, 3, 2);
+
+	vector<bool> visited(v, false);
+
+	// undirected Graph
+	// No Breaks
+	bool ans = false;
+	for(int s=0;s<v;s++) {
+		if(visited[s] == false) {
+			ans = isCycleDetect(adj, visited, s);
+			if(ans == true) {
+				cout << "Cycle Detected\n";
+			}
+		}
+	}
+
+	if(ans == false) {
+		cout << "Cycle not Detected\n";
+	}
+
+
+	return 0;
+}
---
+++ Graph/Detect cycle in an undirected graph/Question.md
@@ -0,0 +1,6 @@
+# Detect cycle in an undirected graph
+
+<p><span style="font-size:18px">Given an undirected graph with V vertices and E edges, check whether it contains any cycle or not. Graph is in the form of adjacency list where adj[i] contains all the nodes ith node is having edge with.<strong><img alt="" src="C:\Users\Mukul kumar\Desktop\GFG_PIC.JPG"></strong></span></p>
+
+<strong> Example 1: </strong>
+<img title="Graph" alt="Alt text" src="../Graph.png">
