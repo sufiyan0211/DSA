4ac045de12f0190f956bfa751e314b5e61f121bc
Mohammed Abdul Sofiyan <sofiyan@Mohammeds-MacBook-Pro.local>
2023-05-23 00:59:38 +0530

    Duplicate subtree in Binary Tree

---
+++ Binary Tree/Duplicate subtree in Binary Tree/Code.cpp
@@ -0,0 +1,29 @@
+class Solution {
+private:
+    string preOrder(Node *root, int &count,
+                    unordered_map<string, int> &subtreeCount) {
+        if(root == NULL) return "N";
+
+        string s = to_string(root->data) + "," +
+                   preOrder(root->left, count, subtreeCount) + "," +
+                   preOrder(root->right, count, subtreeCount);
+
+        if(subtreeCount[s] == 1. && !(root->left == NULL && root->right == NULL)) {
+            count++;
+        }
+
+        subtreeCount[s]++;
+        return s;
+    }
+public:
+    /*This function returns true if the tree contains
+    a duplicate subtree of size 2 or more else returns false*/
+    int dupSub(Node *root) {
+        // code here
+        int count = 0;
+        unordered_map<string, int> subtreeCount;
+        preOrder(root, count, subtreeCount);
+
+        return count;
+    }
+};
---
+++ Binary Tree/Duplicate subtree in Binary Tree/Readme.md
@@ -0,0 +1,7 @@
+Duplicate subtree in Binary Tree
+===
+
+
+|Question|Video|
+|-|-|
+|[GFG](https://practice.geeksforgeeks.org/problems/duplicate-subtree-in-binary-tree/1)||
