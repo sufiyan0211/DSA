4422f38915c7d12cab6494cb6a29a74ff0d72176
Mohammed Abdul Sofiyan <sofiyan@Mohammeds-MacBook-Pro.local>
2023-06-16 01:34:02 +0530

    Refactor code

--- Matrix/Find a specific pair in Matrix/Code.cpp
+++ Matrix/Find a specific pair in Matrix/Code.cpp
@@ -1,55 +1,47 @@
 #include<bits/stdc++.h>
+
 using namespace std;
 
-int maxValueOfdiff(vector<vector<int> > arr, int n) {
-	vector<vector<int> > maxArr(n, vector<int>(n));
+int maxValueOfdiff(vector <vector<int>> mat, int n) {
+    vector <vector<int>> maxArr(n, vector<int>(n));
+    maxArr[n - 1][n - 1] = mat[n - 1][n - 1];
 
-	maxArr[n-1][n-1] = arr[n-1][n-1];
+    // pre compute last column from bottom row to top row
+    for (int currentRow = n - 2; currentRow >= 0; currentRow--) {
+        maxArr[currentRow][n - 1] = max(maxArr[currentRow + 1][n - 1], mat[currentRow][n - 1]);
+    }
 
-	// pre process the last row
-	// bottom row (right to left)
-	int maxValue = arr[n-1][n-1];
-	for(int j=n-2;j>=0;j--) {
-		if(arr[n-1][j] > maxValue) {
-			maxValue = arr[n-1][j];
-		}
-		maxArr[n-1][j] = maxValue;
-	}
 
-	// preProcess the last column
-	// rightmost column (bottom to top)
-	for(int i=n-2;i>=0;i--) {
-		if(arr[i][n-1] > maxValue) {
-			maxValue = arr[i][n-1];
-		}
-		maxArr[i][n-1] = maxValue;
-	}
+    // pre compute last row from right row to left col
+    for (int currentCol = n - 2; currentCol >= 0; currentCol--) {
+        maxArr[n - 1][currentCol] = max(maxArr[n - 1][currentCol + 1], mat[n - 1][currentCol]);
+    }
 
-	maxValue = INT_MIN;
+    int ans = -1e9;
+    for (int row = n - 2; row >= 0; row--) {
+        for (int col = n - 2; col >= 0; col--) {
+            ans = max(ans, maxArr[row + 1][col + 1] - mat[row][col]);
+            maxArr[row][col] = max(mat[row][col],
+                                   max(maxArr[row + 1][col], maxArr[row][col + 1]));
+        }
+    }
 
-	for(int i=n-2;i>=0;i--) {
-		for(int j=n-2;j>=0;j--) {
-			if(maxArr[i+1][j+1] - arr[i][j] > maxValue) {
-				maxValue = maxArr[i+1][j+1] - arr[i][j];
-			}
-			maxArr[i][j] = max(arr[i][j], max(maxArr[i+1][j], maxArr[i][j+1]));
-		}
-	}
 
-	return maxValue;
+    return ans;
 }
 
 int main() {
-	int n; cin >> n;
-	vector<vector<int> > arr(n, vector<int>(n));
-	for(int i=0;i<n;i++) {
-		for(int j=0;j<n;j++) {
-			cin >> arr[i][j];
-		}
-	}
-	int ans = maxValueOfdiff(arr, n);
-	cout << ans << endl;
+    int n;
+    cin >> n;
+    vector <vector<int>> arr(n, vector<int>(n));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> arr[i][j];
+        }
+    }
+    int ans = maxValueOfdiff(arr, n);
+    cout << ans << endl;
 
 
-	return 0;
+    return 0;
 }
