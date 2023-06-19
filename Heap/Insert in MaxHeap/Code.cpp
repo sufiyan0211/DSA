d6693c215425022af4117e92f324aea8fa38243d
Mohammed Abdul Sofiyan <sofiyan@Mohammeds-MacBook-Pro.local>
2023-06-20 01:56:31 +0530

    Modify Logic for MaxHeap

--- Heap/Insert in MaxHeap/Code.cpp
+++ Heap/Insert in MaxHeap/Code.cpp
@@ -1,54 +1,73 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 
 class MaxHeap {
 private:
-	int *arr;
-	int size;
-	int capacity;
+    int *arr;
+    int size;
+    int capacity;
 public:
-	MaxHeap(int capacity, int size) {
-		this->capacity = capacity;
-		this->size = size;
-		arr = new int[capacity];
-	}
+    MaxHeap() {
+        this->capacity = 1e9;
+        this->size = -1;
+        arr = new int[this->capacity];
+    }
 
-	int left(int i) {
-		return (2*i)+1;
-	}
+    int left(int i) {
+        return (2 * i) + 1;
+    }
 
-	int right(int i) {
-		return (2*i)+2;
-	}
+    int right(int i) {
+        return (2 * i) + 2;
+    }
 
-	int parent(int i) {
-		return (i-1)/2;
-	}
+    int parent(int i) {
+        return (i - 1) / 2;
+    }
 
-	int top() {
-		return arr[0];
-	}
+    bool isEmpty() {
+        return (size == -1);
+    }
 
-	void insert(int x) {
-		size++;
-		arr[size-1] = x;
-		int i = size-1;
-		while(arr[i] > arr[parent(i)]) {
-			swap(arr[i], arr[parent(i)]);
-			i = parent(i);
-		}
-	}
+    int top() {
+        if(isEmpty()) throw std::underflow_error("Heap is empty");
+        return arr[0];
+    }
+
+    int getSize() {
+        return size+1;
+    }
+
+    void insert(int val) {
+        if(size >= capacity) throw std::overflow_error("Heap is full");
+        if(isEmpty()) {
+            size++;
+            arr[size] = val;
+            return;
+        }
+
+        size++;
+        arr[size] = val;
+        int i = size;
+        while (arr[i] > arr[parent(i)]) {
+            swap(arr[i], arr[parent(i)]);
+            i = parent(i);
+        }
+    }
 };
 
 int main() {
-	MaxHeap maxHeap(1000, 0);
-	maxHeap.insert(50);
-	cout << maxHeap.top() << endl;
-	maxHeap.insert(60);
-	cout << maxHeap.top() << endl;
-	maxHeap.insert(40);
-	cout << maxHeap.top() << endl;
-	
-	return 0;
+    MaxHeap maxHeap;
+
+    maxHeap.insert(70);
+    cout << maxHeap.top() << endl;
+
+    maxHeap.insert(60);
+    cout << maxHeap.top() << endl;
+
+    maxHeap.insert(80);
+    cout << maxHeap.top() << endl;
+
+    return 0;
 }
