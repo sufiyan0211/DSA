49334955be7e0fac31eb2f54356bde23b6086453
Mohammed Abdul Sofiyan <sofiyan@Mohammeds-MacBook-Pro.local>
2023-06-20 20:01:11 +0530

    Update Deletion In MaxHeap code

--- Heap/Deletion In MaxHeap/Code.cpp
+++ Heap/Deletion In MaxHeap/Code.cpp
@@ -9,10 +9,10 @@ public:
 	int size;
 	int capacity;
 
-	MaxHeap(int capacity) {
-		this->capacity = capacity;
-		this->size = 0;
-		arr = new int[capacity];
+	MaxHeap() {
+		this->capacity = 1e9;
+		this->size = -1;
+		arr = new int[this->capacity];
 	}
 
 	int left(int i) {
@@ -27,14 +27,32 @@ public:
 		return (i-1)/2;
 	}
 
+    bool isEmpty() {
+        return (size == -1);
+    }
+
 	int top() {
+        if(isEmpty()) throw std::underflow_error("Heap is empty");
 		return arr[0];
 	}
 
-	void insert(int x) {
+    int getSize() {
+        return (this->size)+1;
+    }
+
+	void insert(int val) {
+        if(size >= capacity) {
+            throw std::overflow_error("Heap is full");
+            return;
+        }
+        if(size == -1) {
+            size++;
+            arr[size] = val;
+            return;
+        }
 		size++;
-		arr[size-1] = x;
-		int i = size-1;
+		arr[size] = val;
+		int i = size;
 		while(arr[i] > arr[parent(i)]) {
 			swap(arr[i], arr[parent(i)]);
 			i = parent(i);
@@ -60,25 +78,22 @@ public:
 	}
 
 	int extractMin() {
-        if(size == 0) {
+        if(size == -1) {
             cout << "Heap is empty so can't be deleted anything from Heap: ";
             return -1;
         }
-        if(size == 1) {
-            cout << "This the last element of Heap which is deleted: ";
-            size--;
-            return arr[size];
+        if(size == 0) {
+            return arr[size--];
         }
         int ans = arr[0];
-        size--;
-		swap(arr[0], arr[size]);
+		swap(arr[0], arr[size--]);
 		heapify(0);
         return ans;
 	}
 };
 
 int main() {
-	MaxHeap maxHeap(11);
+	MaxHeap maxHeap;
 
 	maxHeap.insert(3);
 	maxHeap.insert(2);
