b1e1ef4ffc10828dbd022eac8348fb0a3e19173a
Mohammed Abdul Sofiyan <sofiyan@Mohammeds-MacBook-Pro.local>
2023-06-21 00:21:24 +0530

    Update code of Delete a node from BST

--- Binary Search Tree/Delete a node from BST/Code.cpp
+++ Binary Search Tree/Delete a node from BST/Code.cpp
@@ -1,15 +1,16 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include <algorithm>
 using namespace std;
 
-class Node{
+class Node {
 public:
-	int data;
-	Node *left, *right;
+    int data;
+    Node *left, *right;
 
-	Node(int data) {
-		this->data = data;
-		this->left = this->right = NULL;
-	}
+    Node(int data) {
+        this->data = data;
+        this->left = this->right = NULL;
+    }
 };
 
 /* Binary Search Tree in the input
@@ -25,103 +26,97 @@ public:
 */
 
 void printTree(Node *root) {
-	if(root != NULL) {
-		printTree(root->left);
-		cout << root->data << " ";
-		printTree(root->right);
-	}
+    if (root != NULL) {
+        printTree(root->left);
+        cout << root->data << " ";
+        printTree(root->right);
+    }
 }
 
-Node* insert(Node* root, int key) {
-    if(root == NULL){
-    	root = new Node(key);
+Node *insert(Node *root, int key) {
+    if (root == NULL) {
+        root = new Node(key);
         return root;
     }
-    if(key < root->data)
-    	root->left = insert(root->left, key);
-    else if(key > root->data)
-    	root->right = insert(root->right, key);
+    if (key < root->data)
+        root->left = insert(root->left, key);
+    else if (key > root->data)
+        root->right = insert(root->right, key);
     return root;
 }
 
 
-Node* minVal(Node* root) {
-    Node *minn = root;
-    while(minn->left != NULL) {
-        minn = minn->left;
+Node *findInorderSuccessor(Node *root) {
+    Node *pre = root->right;
+    while (pre->left != NULL) {
+        pre = pre->left;
     }
-    return minn;
+    return pre;
 }
 
 // Function to delete a node from BST.
 Node *deleteNode(Node *root, int key) {
     // your code goes here
-    if(root == NULL) {
+    if (root == NULL) {
         return root;
     }
-    
-    if(root->data == key) {
-        /* There will 3 cases 
-            1. delete leaf node
-            2. delete node which has either left or right child
-            3. delete node which has both left and right child
-        */
-        
+    /* There will 3 cases
+        1. delete leaf node
+        2. delete node which has either left or right child
+        3. delete node which has both left and right child
+    */
+
+    if (key > root->data) {
+        root->right = deleteNode(root->right, key);
+    } else if (key < root->data) {
+        root->left = deleteNode(root->left, key);
+    } else if (root->data == key) {
         // delete leaf node
-        if(root->left == NULL && root->right == NULL) {
+        if (root->left == NULL && root->right == NULL) {
             delete root;
             return NULL;
         }
-        
-        // delete node which has either left or right child
-        if(root->left != NULL && root->right == NULL) {
+            // delete node which has either left or right child
+        else if (root->left != NULL && root->right == NULL) {
             Node *temp = root->left;
             delete root;
             return temp;
-        }
-        if(root->left == NULL && root->right != NULL) {
+        } else if (root->left == NULL && root->right != NULL) {
             Node *temp = root->right;
             delete root;
             return temp;
         }
-        
-        // delete node which has both left and right child
-        if(root->left != NULL && root->right != NULL) {
-            int minn = minVal(root->right) -> data;
-            root->data = minn;
-            root->right = deleteNode(root->right, minn); // transform to 0 or 1 child deletion 
-            return root;
+            // delete node which has both left and right child
+        else if (root->left != NULL && root->right != NULL) {
+            Node *temp = findInorderSuccessor(root);
+            swap(root->data, temp->data);
+            root->right = deleteNode(root->right, key); // transform to 0 or 1 child deletion
         }
-        
+
     }
-    else if(key > root->data) {
-        root->right = deleteNode(root->right, key);
-    }
-    else {
-        root->left = deleteNode(root->left, key);
-    }
+
     return root;
 }
 
 
-
-
 int main() {
-	int arr[] = {60,30, 90,20,40,80,110,100,120,130};
-	int n = 10; // total nodes 
+    int arr[] = {60, 30, 90, 20, 40, 80, 110, 100, 120, 130};
+    int n = 10; // total nodes
 
-	Node *root = NULL;
-	for(int i=0;i<n;i++) {
-		root = insert(root, arr[i]);
-	}
-	cout << "Printing BST in Preorder Traversal\n";
-	printTree(root); cout << endl;
+    Node *root = NULL;
+    for (int i = 0; i < n; i++) {
+        root = insert(root, arr[i]);
+    }
+    cout << "Printing BST in Inorder Traversal\n";
+    printTree(root);
+    cout << endl;
 
 
-	cout << "Delete node 20\n";
-	deleteNode(root, 20);
-	cout << "Tree after deleting 20\n InorderTraversal\n";
-	printTree(root); cout << endl;
-	
-	return 0;
+    cout << "Delete node 90\n";
+    deleteNode(root, 90);
+    cout << "Tree after deleting 90\n InorderTraversal\n";
+    printTree(root);
+    cout << endl;
+
+    return 0;
 }
