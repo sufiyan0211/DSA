c8dff1691492f372b4cd485f15febf333e6d97e7
Mohammed Abdul Sofiyan <sofiyan@Mohammeds-MacBook-Pro.local>
2023-06-24 18:08:47 +0530

    Update LRU cache code

--- Stack & Queue/LRU Cache Implementation/Code.cpp
+++ Stack & Queue/LRU Cache Implementation/Code.cpp
@@ -1,111 +1,84 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-class Cache {
+class Node {
 public:
-
-	class Node{
-	public:
-		int key;
-		int val;
-		Node *prev;
-		Node *next;
-
-		Node(int key, int val) {
-			this->key = key;
-			this->val = val;
-			prev = NULL;
-			next = NULL;
-		}
-
-	};
-
-	int capacity;
-	unordered_map<int, Node*> mp;
-
-	Node* head = new Node(-1, -1);
-	Node* tail = new Node(-1, -1);
-
-	Cache(int capacity) {
-		this->capacity = capacity;
-		head->next = tail;
-		tail->prev = head;
-	}
-
-	void addElementAfterHead(int key, int val) {
-		Node *newNode = new Node(key, val);
-		mp[key] = newNode;
-
-		Node *nextOfHead = head->next;
-
-		newNode->prev = head;
-		newNode->next = nextOfHead;
-
-		nextOfHead->prev = newNode;
-
-		head->next = newNode;
-		mp[key] = newNode;
-	}
-
-
-	void deleteElement(Node *targetNode) {
-		Node *NodeBeforeTarget = targetNode->prev;
-		Node *NodeAfterTarget = targetNode->next;
-
-		NodeBeforeTarget->next = NodeAfterTarget;
-		NodeAfterTarget->prev = NodeBeforeTarget;
-	}
-
-	void put(int key, int val) {
-		if(mp.size() >= capacity) {
-			// delete the last element from DLL (element just before the tail) and also erase it from map
-			mp.erase(tail->prev->key);
-			deleteElement(tail->prev);
-
-			// Add element after head
-			addElementAfterHead(key, val);
-
-		}
-		else {
-			// add element after head;
-			addElementAfterHead(key, val);
-
-		}
-	}
-
-	int get(int key) {
-		if(mp.find(key) != mp.end()) {
-			Node *targetNode = mp[key];
-			int val = targetNode->val;
-
-			mp.erase(key);
-			deleteElement(targetNode);
-
-			addElementAfterHead(key, val);
-			return val;
-		}
-		else {
-			return -1;
-		}
-	}
-
-
+    int key;
+    int value;
+    Node *prev;
+    Node *next;
+    Node (int key, int value) {
+        this->key = key;
+        this->value = value;
+    }
 };
 
+class LRUCache {
+private:
+    int capacity;
+    Node *head, *tail;
+    unordered_map<int, Node*> mp;
+public:
+    void deleteNode(Node *target) {
+        Node *nodeAfterTarget = target->next;
+        Node *nodeBeforeTarget = target->prev;
 
-int main() {
-	Cache cache(2);
+        nodeAfterTarget->prev = nodeBeforeTarget;
+        nodeBeforeTarget->next = nodeAfterTarget;
 
-	cache.put(1,1);
-	cache.put(2,2);
-	cout << cache.get(1) << endl;
-	cache.put(3,3);
-	cout<< cache.get(2) << endl;
-	cache.put(4,4);
-	cout<< cache.get(1) << endl;
-	cout << cache.get(3) << endl;
-	cout << cache.get(4) << endl;
+        // free(target);
+    }
 
+    Node* addNodeAfterHead(int key, int value) {
+        Node *node = new Node(key, value);
 
-	return 0;
-}
+        Node *nodeAfterHead = head->next;
+
+        head->next = node;
+        node->prev = head;
+        node->next = nodeAfterHead;
+        nodeAfterHead->prev = node;
+        return node;
+    }
+
+    LRUCache(int capacity) {
+        this->capacity = capacity;
+
+        this->head = new Node(-1, -1);
+        this->tail = new Node(-1, -1);
+
+        this->head->next = tail;
+        this->tail->prev = head;
+    }
+
+    int get(int key) {
+        if(mp.find(key) != mp.end()) {
+            Node *target = mp[key];
+            int val = target->value;
+            mp.erase(key);
+
+            deleteNode(target);
+            Node *node = addNodeAfterHead(key, val);
+            mp[key] = node;
+
+            return val;
+        }
+        return -1;
+    }
+
+    void put(int key, int value) {
+        // update the value if key already exist in the cache
+        if(mp.find(key) != mp.end()) {
+            Node *target = mp[key];
+            mp.erase(key);
+            deleteNode(target);
+            Node *node = addNodeAfterHead(key, value);
+            mp[key] = node;
+        }
+        else {
+            Node *node = addNodeAfterHead(key, value);
+            mp[key] = node;
+            if(mp.size() > capacity) {
+                mp.erase(tail->prev->key);
+                deleteNode(tail->prev);
+            }
+        }
+    }
+};
