22bf43449c6af44b6d6040142f72db3dd9b8b0a1
sofiyan <sufiyan0211@gmail.com>
2025-06-08 13:31:01 +0530

    modify logic for implement two stacks in an array

--- Stack & Queue/Implement two stacks in an array/Code.cpp
+++ Stack & Queue/Implement two stacks in an array/Code.cpp
@@ -1,96 +1,55 @@
-#include<bits/stdc++.h>
-
-using namespace std;
-
-class twoStacks
-{
-    int *arr;
+class twoStacks {
+  public:
+  
+    int* arr;
+    int top1;
+    int top2;
     int size;
-    int top1, top2;
-public:
-   twoStacks(int n=100){size = n; arr = new int[n]; top1 = -1; top2 = size;}
- 
-   void push1(int x);
-   void push2(int x);
-   int pop1();
-   int pop2();
+
+    twoStacks(int n=100) {
+        arr = new int[n];
+        top1 = -1;
+        top2 = n;
+        size = n;
+    }
+
+    // Function to push an integer into the stack1.
+    void push1(int x) {
+        // code here
+        if(top1+1 == top2) {
+            return;
+        }
+        arr[++top1] = x;
+    }
+
+    // Function to push an integer into the stack2.
+    void push2(int x) {
+        // code here'
+        if(top2-1 == top1) {
+            return;
+        }
+        arr[--top2] = x;
+    }
+
+    // Function to remove an element from top of the stack1.
+        
+    int pop1() {
+        // code here
+        if(top1 == -1) {
+            // Stack underflow condition
+            return -1;
+        }
+        return arr[top1--];
+    }
+
+        
+    // Function to remove an element from top of the stack2.
+    int pop2() {
+        // code here
+        if(top2 == size) {
+            // Stack underflow condition
+            return -1;
+        }
+        return arr[top2++];
+    }
 };
-
-
-
-int main()
-{
-
-    twoStacks *sq = new twoStacks();
-
-    int Q;
-    cin>>Q;
-    while(Q--) {
-        int stack_no;
-        cin>>stack_no;
-        int QueryType=0;
-        cin>>QueryType;
-
-        if(QueryType==1) {
-            int a;
-            cin>>a;
-            if(stack_no ==1)
-                sq->push1(a);
-            else if(stack_no==2)
-                sq->push2(a);
-        }
-        else if(QueryType==2) {
-        	if(stack_no==1)
-                cout<<sq->pop1()<<" ";
-            else if(stack_no==2)
-                cout<<sq->pop2()<<" ";
-
-        }
-    }
-    cout<<endl;
-}
-
-
-
-//Function to push an integer into the stack1.
-void twoStacks :: push1(int x)
-{
-    if(top1 >= top2 || top1 > size-1) {
-        return;
-    }
-    top1++;
-    arr[top1] = x;
-}
-   
-//Function to push an integer into the stack2.
-void twoStacks ::push2(int x)
-{
-    if(top1 >= top2 || top1 > 99) {
-        return;
-    }
-    top2--;
-    arr[top2] = x;
-}
-   
-//Function to remove an element from top of the stack1.
-int twoStacks ::pop1()
-{
-    if(top1 == -1) {
-        return -1;
-    }
-    int x = arr[top1];
-    top1--;
-    return x;
-}
-
-//Function to remove an element from top of the stack2.
-int twoStacks :: pop2()
-{
-    if(top2 == size) {
-        return -1;
-    }
-    int x = arr[top2];
-    top2++;
-    return x;
-}
-
---
+++ Stack & Queue/Implement two stacks in an array/Readme.md
@@ -0,0 +1,5 @@
+
+
+|Question|Video|
+|---|---|
+|xsz|[Coder Army](https://youtu.be/ktUYEWjqRI8?si=oHOMNNYg75ZSFe4Y)|
