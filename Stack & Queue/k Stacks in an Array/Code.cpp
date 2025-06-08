473d95f50587f2d8911fff3cb714a4f7623bb8fe
sofiyan <sufiyan0211@gmail.com>
2025-06-08 13:20:43 +0530

    modify logic of implement N stack in an array of size of K

--- Stack & Queue/k Stacks in an Array/Code.cpp
+++ Stack & Queue/k Stacks in an Array/Code.cpp
@@ -1,76 +1,122 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+
 using namespace std;
 
-class kStacks {
-private:
-	int freespot;
-	int k;
-	int n;
-	int *arr;
-	int *top;
-	int *next;
+class Node {
 public:
-	kStacks(int n, int k) {
-		freespot = 0;
-		this->k = k;
-		this->n = n;
-		arr = new int[n];
-		top = new int[k];
-		next = new int[n];
+	int index;
+	Node *next;
 
-		for(int i=0;i<k;i++) top[i] = -1;
-		for(int i=0;i<n;i++) next[i] = (i+1);
-		next[n-1] = -1;
+	Node(int x) {
+		index = x;
+		next = NULL;
+	}
+};
+
+class Nstack {
+public:
+	int* arr; // original array
+	Node** topp; // top element index of particular array
+	Stack<int> emptyIndexes; // Empty index 
+
+	// n is number of stack and s size of an array
+	Nstack(int n, int s) {
+		arr = new int[s];
+		topp = new Node*[n];
+
+		for(int i=0; i<n; i++) {
+			topp[i] = NULL;
+		}
+		for(int i=0; i<s ;i++) {
+			emptyIndexes.push(i);
+		}
 	}
 
-	bool push(int x, int s) {
-		if(freespot == -1) return false; // stack overflow
+	// push element x in mth stack
+	bool push(int x, int m) {
+		if(emptyIndexes.empty()) {
+			return false;
+		}
 
-		int index = freespot;
-		arr[index] = x;
-		freespot = next[index];
-		next[index] = top[s];
-		top[s] = index;
+		int availableIndex = emptyIndexes.top();
+
+		arr[availableIndex] = x;
+
+		Node *temp = new Node(availableIndex);
+		temp->next = topp[m-1];
+		topp[m-1] = temp;
+
+
+		emptyIndexes.pop();
+
 		return true;
 	}
 
-	int pop(int s) {
-		if(top[s] == -1) return -1; // stack underflow
+	// pop element from mth Stack
+	int pop(int m) {
+		if(topp[m-1] == NULL) {
+			return -1;
+		}
 
-		int index = top[s];
-		top[s] = next[index];
-		next[index] = freespot;
-		freespot = index;
+		int availableIndex = topp[m-1]->index;
 
-		return arr[index];
+		int element = arr[topp[m-1]->index];
+		topp[m-1] = topp[m-1]->next;
+
+		emptyIndexes.push(availableIndex);
+
+		return element;
+	}
+
+	// return top element of mth Stack
+	int top(int m) {
+		if(topp[m-1] == NULL) {
+			return -1;
+		}
+
+		return arr[topp[m-1]->index];
 	}
 
 };
 
 
 int main() {
-	int n,k; cin >> n >> k;
 
-	kStacks ks(n,k);
+	Nstack nStack(3, 20); // 3 stacks in an array of size 20
 
-	// let ip n=10 and k=3;
+	nstack.push(11, 1);
+	nstack.push(12, 2);
+	nstack.push(13, 3);
+	nstack.push(14, 1);
+	nstack.push(15, 2);
+	nstack.push(16, 3);
+	nstack.push(17, 1);
+	nstack.push(18, 2);
+	nstack.push(19, 3);
+	nstack.push(20, 1);
+	nstack.push(21, 2);
+	nstack.push(22, 3);
 
-	ks.push(15, 2);
-    ks.push(45, 2);
-  
-    // Let us put some items in stack number 1
-    ks.push(17, 1);
-    ks.push(49, 1);
-    ks.push(39, 1);
-  
-    // Let us put some items in stack number 0
-    ks.push(11, 0);
-    ks.push(9, 0);
-    ks.push(7, 0);
-  
-    cout << "Popped element from stack 2 is " << ks.pop(2) << endl;
-    cout << "Popped element from stack 1 is " << ks.pop(1) << endl;
-    cout << "Popped element from stack 0 is " << ks.pop(0) << endl;
+	cout << "operations on stack-1" << endl;
+	cout << nStack.top(1) << endl;
+
+	nStack.pop(1);
+	cout << nStack.top(1) << endl;
+
+
+	nStack.pop(1);
+	cout << nStack.top(1) << endl;
+
+
+	cout << "operations on stack-2" << endl;
+	cout << nStack.top(2) << endl;
+
+	nStack.pop(2);
+	cout << nStack.top(2) << endl;
+
+
+	nStack.pop(2);
+	cout << nStack.top(2) << endl;
 
 	return 0;
 }
---
+++ Stack & Queue/k Stacks in an Array/Readme.md
@@ -0,0 +1,2 @@
+|Question|Video|
+||[Coder Army](https://youtu.be/ktUYEWjqRI8?si=oHOMNNYg75ZSFe4Y)|
