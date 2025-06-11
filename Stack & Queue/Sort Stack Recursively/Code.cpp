bae2945c56af2e7751349f075c4b0c03b43e2c50
sofiyan <sufiyan0211@gmail.com>
2025-06-11 14:07:08 +0530

    modify question: Sort a stack

--- Stack & Queue/Sort Stack Recursively/Code.cpp
+++ Stack & Queue/Sort Stack Recursively/Code.cpp
@@ -1,56 +1,31 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-
-void insertAtTheBottom(stack<int> &s, int value) {
-	if(s.empty() == true) {
-		s.push(value);
-		return;
-	}
-	else {
-		int temp = s.top();
-		s.pop();
-		insertAtTheBottom(s, value);
-		s.push(temp);
-	}
-}
-
-void insertAtCorrectPos(stack<int> &s, int val) {
-    if(s.empty() == true || val >= s.top()) {
-        s.push(val);
+void insertInSortedStack(stack<int> &s, int element) {
+    if(s.empty() || (s.top() <= element)) {
+        s.push(element);
+        return;
     }
-    else {
-        int temp = s.top();
-        s.pop();
-        insertAtCorrectPos(s, val);
-        s.push(temp);
+    
+    int topp = s.top();
+    s.pop();
+    
+    // recursive call
+    insertInSortedStack(s, element);
+    
+    s.push(topp);
+}
+
+
+void sortStack(stack<int> &s) {
+    // Your code here
+    if(s.empty()) {
+        return;
     }
-}
-
-void sort(stack<int> &s) {
-	if(s.empty() == true) return; 
-	int temp = s.top();
-	s.pop();
-	sort(s);
-	insertAtCorrectPos(s,temp);
-}
-
-
-int main() {
-	stack<int> s;
-
-	int n; cin >>n;
-	for(int i=0;i<n;i++) {
-		int val; cin >> val;
-		s.push(val);
-	}
-	
-	sort(s);
-
-	while(!s.empty()) {
-		cout << s.top() << " ";
-		s.pop();
-	} cout << endl;
-
-	return 0;
+    
+    
+    int topp = s.top();
+    s.pop();
+    
+    // recursive call
+    sortStack(s);
+    
+    insertInSortedStack(s, topp);
 }
