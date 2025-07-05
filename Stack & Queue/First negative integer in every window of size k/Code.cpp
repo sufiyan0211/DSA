e08125c8193b662d63f4ced853047f34f623db37
Mohammed Abdul Sofiyan <32041884+sufiyan0211@users.noreply.github.com>
2025-07-05 23:00:52 +0530

    Update Code.cpp

--- Stack & Queue/First negative integer in every window of size k/Code.cpp
+++ Stack & Queue/First negative integer in every window of size k/Code.cpp
@@ -1,44 +1,27 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-vector<int> printFirstNegativeInteger(int arr[], int n, int k) {
-    vector<int> ans(n-k+1);
-    deque<int> dq;
-    for(int i=0;i<k;i++) {
-        if(arr[i] < 0) {
-            dq.push_back(i);
+class Solution {
+  public:
+    vector<int> firstNegInt(vector<int>& arr, int k) {
+        // write code here
+        
+        int n = arr.size();
+        deque<int> dq;
+        vector<int> ans;
+        
+        for(int i=0; i<k; i++) {
+            if(arr[i] < 0) dq.push_back(i);
         }
+        
+        if(dq.empty()) ans.push_back(0);
+        else ans.push_back(arr[dq.front()]);
+        
+        
+        for(int i=k; i<n; i++) {
+            if(arr[i] < 0) dq.push_back(i);
+            while(!dq.empty() && i-dq.front() >= k) dq.pop_front();
+            if(dq.empty()) ans.push_back(0);
+            else ans.push_back(arr[dq.front()]);
+        }
+        
+        return ans;
     }
-    ans[0] = (dq.empty()) ? 0 : arr[dq.front()];
-    int j = 1;
-    for(int i=k;i<n;i++) {
-        if(arr[i] < 0) {
-            dq.push_back(i);
-        }
-        while(!dq.empty() && dq.front() <= (i-k)) {
-            dq.pop_front();
-        }
-        ans[j++] = (dq.empty()) ? 0 : arr[dq.front()];
-    }
-    return ans;
 }
-
-
-int main() {
-
-    int n;
-    cin >> n;
-    int arr[n];
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
-    int k;
-    cin >> k;
-
-    vector<int> ans = printFirstNegativeInteger(arr, n, k);
-    for (auto it : ans) cout << it << " ";
-    cout << endl;
-    return 0;
-}
-
-
