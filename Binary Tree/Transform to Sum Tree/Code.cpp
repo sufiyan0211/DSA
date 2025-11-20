559f9f99b209b6424986c03dc9751acb1bcb6f0e
Mohammed Abdul Sofiyan <32041884+sufiyan0211@users.noreply.github.com>
2025-11-20 15:12:20 +0530

    Refactor sum tree conversion logic

--- Binary Tree/Transform to Sum Tree/Code.cpp
+++ Binary Tree/Transform to Sum Tree/Code.cpp
@@ -1,23 +1,23 @@
 class Solution {
-public:
+  public:
 
     // Convert a given tree to a tree where every node contains sum of values of
     // nodes in left and right subtrees in the original tree
-
-    int sumTree(Node *root) {
-        if(root == NULL) {
-            return 0;
-        }
-        int oldValueOfRoot = root->data;
-        int left = sumTree(root->left);
-        int right = sumTree(root->right);
-        root->data = left+right;
-        return root->data + oldValueOfRoot;
+    int sumTreeUtility(Node *root) {
+        if (root == NULL) return 0;
+        
+        int leftSubtree = sumTreeUtility(root->left);
+        int rightSubtree = sumTreeUtility(root->right);
+        
+        int ans = root->data + leftSubtree + rightSubtree;
+        
+        root->data = leftSubtree + rightSubtree;
+        
+        return ans;
     }
-
-    void toSumTree(Node *root)
-    {
+    
+    void toSumTree(Node *root) {
         // Your code here
-        sumTree(root);
+        sumTreeUtility(root);
     }
 };
