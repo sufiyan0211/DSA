d3c22f7fea0480606fff2620056965c0d4f03e57
Mohammed Abdul Sofiyan <32041884+sufiyan0211@users.noreply.github.com>
2025-11-20 11:13:24 +0530

    Refactor boundary traversal into Solution class
    Refactored boundary traversal functions into Solution class and updated logic for right traversal using a stack.

--- Binary Tree/Boundary Traversal of binary tree/Code.cpp
+++ Binary Tree/Boundary Traversal of binary tree/Code.cpp
@@ -1,75 +1,3 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-class Node{
-public:
-    int key;
-    Node *left;
-    Node *right;
-    Node(int key) {
-        this->key = key;
-        left = right = NULL;
-    }
-};
-
-void leftTraversal(Node *root, vector<int> &ans) {
-    if(root == NULL || (root->left == NULL && root->right == NULL))  
-        return;
-    ans.push_back(root->key);
-    if(root->left) {
-        leftTraversal(root->left, ans);
-    }
-    else {
-        leftTraversal(root->right, ans);
-    }
-}
-// preOrderTraversal
-void leafTraversal(Node *root, vector<int> &ans) {
-    if(root == NULL) return;
-    if(root->left == NULL && root->right == NULL) {
-        ans.push_back(root->key);
-    }
-    leafTraversal(root->left, ans);
-    leafTraversal(root->right, ans);
-}
-
-// postorderTraversal
-void rightTraversal(Node *root, vector<int> &ans) {
-    if(root == NULL || (root->left == NULL && root->right == NULL))  
-        return;
-        
-    if(root->right != NULL) {
-        rightTraversal(root->right, ans);
-    }
-    else {
-        rightTraversal(root->left, ans);
-    }
-    
-    ans.push_back(root->key);
-}
-vector <int> boundary(Node *root)
-{
-    //Your code here
-    vector<int> ans;
-    if(root == NULL) return ans;
-    
-    // store root
-    ans.push_back(root->key);
-    
-    // store left boundries (except: root, leaf Nodes)
-    leftTraversal(root->left, ans);
-    
-    // store left leaf nodes
-    leafTraversal(root->left, ans);
-    
-    // store right leaf nodes
-    leafTraversal(root->right, ans);
-    
-    // store right boundries
-    rightTraversal(root->right, ans);
-    
-    return ans;
-}
 /* Binary Tree in the input
             10
            /   \
@@ -85,23 +13,64 @@ vector <int> boundary(Node *root)
 
 */
 
-int main() {
-    Node *root = new Node(10);
-    root->left = new Node(20);
-    root->right = new Node(30);
-    root->left->left = new Node(40);
-    root->left->right = new Node(50);
-    root->right->left = new Node(60);
-    root->right->right = new Node(70);
-    root->right->right->left = new Node(80);
-    root->right->right->left->left = new Node(90);
-    root->right->right->left->left->right = new Node(100);
-
-
-    vector<int> ans = boundary(root);
-    for(auto s: ans) {
-        cout << s << " ";
-    } cout << endl;
-
-    return 0;
-}
+class Solution {
+  public:
+  
+    void leftTraversal(Node *root, vector<int> &ans) {
+        if (root == NULL || (root->left == NULL && root->right == NULL)) return;
+        
+        ans.push_back(root->data);
+        
+        if(root->left) leftTraversal(root->left, ans);
+        else leftTraversal(root->right, ans);
+    }
+    
+    void leafTravesal(Node *root, vector<int> &ans) {
+        if (root == NULL) return;
+        
+        if(root->left == NULL && root->right == NULL) ans.push_back(root->data);
+        
+        leafTravesal(root->left, ans);
+        leafTravesal(root->right, ans);
+    }
+    
+    
+    void rightTraversal(Node *root, stack<int> &rightNodesStack) {
+        if (root == NULL || (root->left == NULL && root->right == NULL)) return;
+        
+        rightNodesStack.push(root->data);
+        
+        if(root->right) rightTraversal(root->right, rightNodesStack);
+        else rightTraversal(root->left, rightNodesStack);
+    }
+    
+    
+    vector<int> boundaryTraversal(Node *root) {
+        // code here
+        vector<int> ans;
+        
+        if (root == NULL) return ans;
+        
+        // This for to handle [1 N N]
+        if(root->left == NULL && root->right == NULL) {
+            ans.push_back(root->data);
+            return ans;
+        }
+        
+        // push root ka data
+        ans.push_back(root->data);
+        
+        leftTraversal(root->left, ans);
+        leafTravesal(root, ans);
+        
+        stack<int> rightNodesStack;
+        rightTraversal(root->right, rightNodesStack);
+        
+        while(!rightNodesStack.empty()) {
+            ans.push_back(rightNodesStack.top());
+            rightNodesStack.pop();
+        }
+        
+        return ans;
+    }
+};
