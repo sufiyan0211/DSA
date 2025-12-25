cc2685f878a596fc281b2c6254daf3865bfd1757
Mohammed Abdul Sofiyan <32041884+sufiyan0211@users.noreply.github.com>
2025-12-25 18:10:45 +0530

    Refactor subset sum function into Solution class

--- Dynamic Programming/Subset Sum Equal To K/Tabulation.cpp
+++ Dynamic Programming/Subset Sum Equal To K/Tabulation.cpp
@@ -1,39 +1,31 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
-
-
-bool subsetSumToK(int n, int k, vector<int> &arr) {
-    // Write your code here.
-    vector<vector<bool>> dp(n, vector<bool> (k+1, false));
-
-    for(int i=0;i<n;i++) {
-        dp[i][0] = true;
-    }
-
-    if(arr[n-1] <= k)
+class Solution {
+  public:
+    
+    bool isSubsetSum(vector<int>& arr, int sum) {
+        // code here
+        int n = arr.size();
+        
+        vector<vector<bool>> dp(n, vector<bool> (sum+1, false));
+        
+        for(int i=0; i<n; i++) {
+            dp[i][0] = true;
+        }
+    
         dp[n-1][arr[n-1]] = true;
 
-    for(int i=n-2; i>=0; i--) {
-        for(int target=1; target<=k; target++) {
-            bool pick = false;
-            if(target-arr[i] >= 0) {
-                pick = dp[i+1][target-arr[i]];
+        
+        // Fill bottom-up
+        for(int i = n - 2; i >= 0; i--) {
+            for(int s = 1; s <= sum; s++) {
+                bool notPick = dp[i + 1][s];
+                bool pick = false;
+                if(s - arr[i] >= 0) {
+                    pick = dp[i + 1][s - arr[i]];
+                }
+                dp[i][s] = pick || notPick;
             }
-            bool notPick = dp[i+1][target];
-
-            dp[i][target] = (pick || notPick);
         }
+
+        return dp[0][sum];
     }
-
-    return dp[0][k];
-}
-
-
-int main() {
-    vector<int> arr = {4, 3, 2, 1};
-    int n = 4, k = 5;
-    cout << subsetSumToK(n, k, arr) << endl;
-    return 0;
-}
+};
