50f557aad9972bf37944f03def28d87c698b2170
Mohammed Abdul Sofiyan <32041884+sufiyan0211@users.noreply.github.com>
2025-12-27 17:10:42 +0530

    Update Tabulation.cpp

--- Dynamic Programming/Partition a set into two subsets such that the difference of subset sums is minimum/Tabulation.cpp
+++ Dynamic Programming/Partition a set into two subsets such that the difference of subset sums is minimum/Tabulation.cpp
@@ -1,38 +1,39 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
-int minSubsetSumDifference(vector<int>& arr, int n)
-{
-    // Write your code here.
-    int totalSum = 0;
-    for(int i=0;i<n;i++) {
-        totalSum += arr[i];
-    }
-
-    int k = totalSum;
-    vector<vector<bool>> dp(n, vector<bool> (k+1, false));
-    for(int i=0;i<n;i++) {
-        dp[i][0] = true;
-    }
-    if(arr[n-1] <= k)
-        dp[n-1][arr[n-1]] = true;
-
-    for(int i=n-2; i>=0; i--) {
-        for(int target=1; target<=k; target++) {
-            bool pick = false;
-            if(target-arr[i] >= 0) {
-                pick = dp[i+1][target-arr[i]];
-            }
-            bool notPick = dp[i+1][target];
-            dp[i][target] = (pick || notPick);
+class Solution {
+public:
+    int minDifference(vector<int>& arr) {
+        int n = arr.size();
+        
+        int sum = 0;
+        for(int i=0; i<n; i++) {
+            sum += arr[i];
         }
+        
+        vector<vector<bool>> dp(n, vector<bool>(sum+1, false));
+        
+        // Base case: sum 0 is always achievable
+        for(int i=0; i<n; i++) {
+            dp[i][0] = true;
+        }
+        
+        // Base case: at last index, can achieve arr[n-1] by picking it
+        dp[n-1][arr[n-1]] = true;
+        
+        // Fill DP table
+        for(int i=n-2; i>=0; i--) {
+            for(int s=1; s<=sum; s++) {
+                bool notPick = dp[i+1][s];
+                bool pick = false;
+                if(s >= arr[i]) pick = dp[i+1][s-arr[i]];
+                dp[i][s] = notPick || pick;
+            }
+        }
+        
+        // Find minimum difference
+        int ans = 1e9;
+        for(int s=0; s<=sum/2; s++) {
+            if(dp[0][s]) ans = min(ans, abs(s - (sum - s))); 
+        }
+        
+        return ans;
     }
-
-    int mini = 1e9;
-    for(int s1=0; s1<=totalSum/2; s1++) {
-        if(dp[0][s1] == true)
-            mini = min(mini, abs(totalSum-s1-s1));
-    }
-    return mini;
-}
+};
