d1566090f77099d529ac76621ec1da15b1a8b6d3
Mohammed Abdul Sofiyan <32041884+sufiyan0211@users.noreply.github.com>
2025-12-30 13:33:41 +0530

    Refactor knapsack function to class-based structure

--- Dynamic Programming/01 Knapsack Problem/Tabulation.cpp
+++ Dynamic Programming/01 Knapsack Problem/Tabulation.cpp
@@ -1,30 +1,35 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
-int knapsack(vector<int> &weight, vector<int> &value, int n, int maxWeight)
-{
-    // Write your code here
-    vector<vector<int>> dp(n, vector<int> (maxWeight+1, 0));
-
-    if(weight[n-1] <= maxWeight) dp[n-1][weight[n-1]] = 1;
-
-    for(int i=n-1;i>=0;i--) {
-        for(int w=0; w<=maxWeight; w++) {
-            if(i == n-1) {
-                if(weight[n-1] <= w) dp[i][w] = value[n-1];
-                continue;
+class Solution {
+  public:
+    
+    int knapsack(int w, vector<int> &val, vector<int> &wt) {
+        // code here
+        
+        int n = val.size();
+        
+        vector<vector<int>> dp(n+1, vector<int> (w+1, 0));
+        
+        // // w == 0
+        // for(int i=0; i<=n; i++) {
+        //     dp[i][0] = 0;
+        // }
+        
+        // // index == n
+        // for(int i=0; i<=w; i++) {
+        //     dp[n][i] = 0;
+        // }
+        
+        for(int i=n-1; i>=0; i--) {
+            for(int j=0; j<= w; j++) {
+                int notPick = dp[i+1][j];
+        
+                int pick = 0;
+                if(j-wt[i] >= 0) pick = val[i] + dp[i+1][j-wt[i]];
+                
+                dp[i][j] = max(pick, notPick);
             }
-
-            // include
-            int include = 0;
-            if(w-weight[i] >= 0)
-                include = value[i] + dp[i+1][w-weight[i]];
-
-            // Not include weight[i]
-            int exclude = dp[i+1][w];
-            dp[i][w] = max(include, exclude);
         }
+        
+        
+        return dp[0][w];
     }
-    return dp[0][maxWeight];
-}
+};
