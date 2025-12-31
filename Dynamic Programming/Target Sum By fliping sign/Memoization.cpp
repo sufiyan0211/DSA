a06361e71072918f83f2d341e9df9d66ed950a79
Mohammed Abdul Sofiyan <32041884+sufiyan0211@users.noreply.github.com>
2025-12-31 17:09:55 +0530

    Refactor target sum solution with memoization

--- Dynamic Programming/Target Sum By fliping sign/Memoization.cpp
+++ Dynamic Programming/Target Sum By fliping sign/Memoization.cpp
@@ -1,26 +1,36 @@
-#include <iostream>
-#include <vector>
-using namespace std;
+class Solution {
+public:
+    int findTargetSumWays(int i, int target, vector<int>& a, vector<vector<int>> &dp) {
 
-int count(int i, int target, int n, vector<int>& arr,
-          vector<vector<int>> &dp) {
-    if(i == n) {
-        if(target == 0) return 1;
-        else return 0;
+        if(i == 0) {
+            if(target == 0 && a[0] == 0) return 2;
+            if(target == 0 || a[0] == target) return 1;
+            return 0;
+        }
+
+        if(dp[i][target] != -1) return dp[i][target];
+
+        int notPick = findTargetSumWays(i-1, target, a, dp);
+
+        int pick = 0;
+        if(target-a[i] >= 0) pick = findTargetSumWays(i-1, target-a[i], a, dp);
+
+        return dp[i][target] = pick+notPick;
     }
-    if(dp[i][target] != -1) return dp[i][target];
-    int include = 0;
-    if(target-arr[i] >= 0) include = count(i+1, target-arr[i], n, arr, dp);
-    int exclude = count(i+1, target, n, arr, dp);
-    return dp[i][target] = include+exclude;
-}
 
-int targetSum(int n, int target, vector<int>& arr) {
-    // Write your code here.
-    int sum = 0;
-    for(auto it : arr) sum += it;
-    if(sum-target < 0 || (sum-target)%2 != 0) return false;
-    int t = ((sum-target)/2)+1;
-    vector<vector<int>> dp(n, vector<int> (t, -1));
-    return count(0, (sum-target)/2, n, arr, dp);
-}
+
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n = nums.size();
+
+        int sum = 0;
+        for(auto x: nums) sum += x;
+
+        if(sum < abs(target) || (sum+target)%2 != 0) return 0;
+
+        int s1 = (sum+target) / 2;
+
+        vector<vector<int>> dp(n, vector<int> (s1+1, -1));
+
+        return findTargetSumWays(n-1, s1, nums, dp);
+    }
+};
